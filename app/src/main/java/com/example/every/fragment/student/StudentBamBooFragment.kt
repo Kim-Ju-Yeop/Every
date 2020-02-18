2e64b495195e2afc3de40e7c4166c865008139b6
김주엽 <49600974+Kim-Ju-Yeop@users.noreply.github.com>
2020-02-18 23:01:32 +0900

    [ADD] Bamboo Item Layout 구성

--- app/src/main/java/com/example/every/fragment/student/StudentBamBooFragment.kt
+++ app/src/main/java/com/example/every/fragment/student/StudentBamBooFragment.kt
@@ -8,10 +8,10 @@ import android.view.ViewGroup
 
 import com.example.every.R
 
+
 class StudentBambooFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_student_bamboo, container, false)
     }
 }

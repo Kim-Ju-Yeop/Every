b948dc5907930e686fdb63f67445fb5e8cdd1958
김주엽 <49600974+Kim-Ju-Yeop@users.noreply.github.com>
2020-03-13 03:00:32 +0900

    [ADD] 로그아웃 기능 추가

---
+++ app/src/main/java/com/example/every/viewmodel/student/activity/more/StudentAccountViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.every.viewmodel.student.activity.more
+
+import android.util.Log
+import android.widget.TextView
+import com.example.every.base.BaseViewModel
+import com.example.every.base.StudentData
+import com.example.every.network.Data
+import com.example.every.network.Response
+import com.example.every.widget.SingleLiveEvent
+import retrofit2.Call
+import retrofit2.Callback
+
+class StudentAccountViewModel : BaseViewModel(){
+
+    /**
+     * getStudentInfo 학생 정보 조회 API Response
+     * status[200] 멤버 조회 성공
+     */
+
+    val onLogoutEvent = SingleLiveEvent<Unit>()
+
+    fun getStudentInfo(studentName : TextView, studentEmail : TextView, studentPhone : TextView, studentSchool : TextView, studentBirth : TextView){
+        val res : Call<Response<Data>> = netRetrofit.bamboo.getStudentInfo(StudentData.token.value.toString(), StudentData.studentIdx.value!!)
+        res.enqueue(object : Callback<Response<Data>> {
+            override fun onResponse(call: Call<Response<Data>>, response: retrofit2.Response<Response<Data>>) {
+                if(response.code() == 200){
+                    studentName.text = response.body()!!.data!!.member!!.name
+                    studentEmail.text = response.body()!!.data!!.member!!.email
+                }
+            }
+            override fun onFailure(call: Call<Response<Data>>, t: Throwable) {
+                Log.e("getStudentInfo[Error]", "학생 정보 조회 과정에서 서버와 통신이 되지 않았습니다.")
+            }
+        })
+    } fun logout() = onLogoutEvent.call()
+}
